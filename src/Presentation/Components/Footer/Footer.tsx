7d0131925e0531cdc35cbb6bc11c3deef9005c8e
Diego Vieira <dvsdiegovieira@gmail.com>
2024-02-21 12:25:38 -0300

    feat: footer

---
+++ src/Presentation/Components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+import useViewModel from "../../Todo/TodoList/HomeViewModel";
+import './Footer.css'
+export default function Footer() {
+    return (
+        <footer>
+            
+        </footer>
+    );
+}
