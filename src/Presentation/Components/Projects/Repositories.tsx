927b4efa95ca1d01919e56f534b6fd9e00749394
Diego Vieira <dvsdiegovieira@gmail.com>
2024-02-21 12:27:36 -0300

    update: repositories

--- src/Presentation/Components/Projects/Repositories.tsx
+++ src/Presentation/Components/Projects/Repositories.tsx
@@ -1,25 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Repositories.css'
 import useRepositoriesModel from "./RepositoriesModel";
 
-export default function HomeView() {
+export default function Projects() {
     const {
         getProjects,
         onChangeValue,
         projects,
     } = useRepositoriesModel();
+    useEffect(()=>{
+        getProjects()
+    },[])
     
     return (
         <article>
             <header>
-                <h2>Repositories</h2>
+                <h2>Projects</h2>
             </header>
             <ul>
                 {projects.map(item => (
-                    <li>{item.title}</li>
+                    <li key={item.id}>{item.title}</li>
                 ))}
             </ul>
+            <footer>
 
+            </footer>
         </article>
     );
 }
