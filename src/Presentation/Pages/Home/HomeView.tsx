7f49a39396ea7809197ddd8930d721f648949c94
Diego Vieira <dvsdiegovieira@gmail.com>
2024-02-22 09:00:37 -0300

    feat: new style components

--- src/Presentation/Pages/Home/HomeView.tsx
+++ src/Presentation/Pages/Home/HomeView.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import './HomeView.css'
 import { TbBrandGithubFilled } from "react-icons/tb";
 import AboutMe from "../../Components/AboutMe/AboutMe";
-import Projects from "../../Components/Projects/Repositories";
+import Projects from "../../Components/Projects/Projects";
 import Footer from "../../Components/Footer/Footer";
 export default function HomeView() {
 
@@ -11,14 +11,16 @@ export default function HomeView() {
     <main>
       <aside></aside>
       <section id="wellcome-section">
-        <h1>Hello,<br/>
-           nice to see you</h1>
+        <h1 id="hello">Hello</h1>
+        <h1>nice to see you</h1>
         <h2>These are my private repositories</h2>
         <h3>Feel free to look around</h3>
+     
+        <div className="cicle"></div>
       </section>
-      <AboutMe/>
-      <Projects/>
-      <Footer/>
+      <AboutMe />
+      <Projects />
+      <Footer />
     </main>
   );
 }
