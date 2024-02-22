dfb882a746ca178e4ff0ce7e43de1e81802e39df
Diego Vieira <dvsdiegovieira@gmail.com>
2024-02-22 09:01:12 -0300

    feat: contacts

---
+++ src/Presentation/Components/Contact/Contact.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+import useViewModel from "../../Todo/TodoList/HomeViewModel";
+import './Contact.css'
+export default function Contact() {
+    return (
+        <section>
+            <h2>Contact Me</h2>
+        </section>
+    );
+}
