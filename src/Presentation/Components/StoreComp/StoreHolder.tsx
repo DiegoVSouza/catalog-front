92f2f18748171645aa105a62340872d3d0e3fdd7
Diego Vieira <dvsdiegovieira@gmail.com>
2024-04-07 16:27:19 -0300

    feat: stores page

---
+++ src/Presentation/Components/StoreComp/StoreHolder.tsx
@@ -0,0 +1,14 @@
+import './Stores.css'
+import { Box } from "@chakra-ui/react";
+
+export default function StoreHolder() {
+
+  return (
+
+    <main>
+      <Box className="banner" height='10rem'>
+      
+      </Box>
+    </main>
+  );
+}
--- src/main/routes/app.routes.tsx
+++ src/main/routes/app.routes.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomeView from '../../Presentation/Pages/Home/HomeView';
 import LoginPage from '../../Presentation/Pages/Login/LoginView';
+import Stores from '../../Presentation/Pages/Stores/Stores';
 
 const Router: React.FC = ()=> {
   return (
     <Routes>
       <Route path='/home/' element={<HomeView/>}/>
+      <Route path='/stores/' element={<Stores/>}/>
       <Route path='/login/' element={<LoginPage/>}/>
     </Routes>
   );
